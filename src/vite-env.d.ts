68055fd8029f869f2cdf550900bcb6dd2d835f1a
Eshrak Rahman <eshrakurrahman@gmail.com>
2026-05-24 14:39:40 +0600

    a

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,10 @@
-VITE_API = "https://api.escuelajs.co/api/v1/";
-VITE_API_BASE_URL = "http://ecom-api.test/api";
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_API_BASE_URL: string;
-    // add other variables here as your project grows
+  readonly VITE_API_BASE: string;
+  readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
